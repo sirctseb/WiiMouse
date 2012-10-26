96851e3daad9d0de960c5f1caafb36c19cd24a95
Christopher Best <bestchris@gmail.com>
2012-10-26 14:50:49 -0400

    started class for status display'

---
+++ src/edu/umich/cjbest/wiimouse/StatusWindow.java
@@ -0,0 +1,12 @@
+package edu.umich.cjbest.wiimouse;
+
+import javax.swing.JPanel;
+
+public class StatusWindow extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
