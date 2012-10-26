80f29c5cbc3889a3767cf3d401dc5415fa8c0afb
Christopher Best <bestchris@gmail.com>
2012-10-26 16:09:50 -0400

    disable psm minimum; set LED on connect; check type of device returned

--- src/edu/umich/cjbest/wiimouse/WiiMouse.java
+++ src/edu/umich/cjbest/wiimouse/WiiMouse.java
@@ -1,5 +1,7 @@
 package edu.umich.cjbest.wiimouse;
 
+import java.io.IOException;
+
 import org.jdesktop.application.Application;
 import org.jdesktop.application.SingleFrameApplication;
 
@@ -19,12 +21,14 @@ public class WiiMouse extends SingleFrameApplication implements WiiDeviceDiscove
 		remoteHandler = new WiiRemoteEventHandler(this);
 		// search for remotes
 		WiiRemoteJ.findRemotes(this, 1);
+		System.out.println("started finding remotes");
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.setProperty("bluecove.jsr82.psm_minimum_off", "true");
 		Application.launch(WiiMouse.class, args);
 	}
 
@@ -32,15 +36,31 @@ public class WiiMouse extends SingleFrameApplication implements WiiDeviceDiscove
 	
 	@Override
 	public void findFinished(int arg0) {
+		System.out.println("finished finding devices");
 		// NOOP
 	}
 
 	@Override
 	public void wiiDeviceDiscovered(WiiDeviceDiscoveredEvent arg0) {
-		// store remote
-		remote = (WiiRemote)arg0.getWiiDevice();
-		remote.addWiiRemoteListener(remoteHandler);
-		System.out.println("device discovered");
+		if(arg0.getWiiDevice() instanceof WiiRemote) {
+			System.out.println("discovered device");
+			// store remote
+			remote = (WiiRemote)arg0.getWiiDevice();
+			remote.addWiiRemoteListener(remoteHandler);
+			System.out.println("added remote listener");
+			try {
+				remote.setLEDIlluminated(0, true);
+				remote.setUseMouse(false);
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("discovered non-WiiRemote");
+		}
 	}
 
 }
--- src/edu/umich/cjbest/wiimouse/WiiRemoteEventHandler.java
+++ src/edu/umich/cjbest/wiimouse/WiiRemoteEventHandler.java
@@ -1,6 +1,8 @@
 package edu.umich.cjbest.wiimouse;
 
+import wiiremotej.event.WRButtonEvent;
 import wiiremotej.event.WRIREvent;
+import wiiremotej.event.WRStatusEvent;
 import wiiremotej.event.WiiRemoteAdapter;
 
 public class WiiRemoteEventHandler extends WiiRemoteAdapter {
@@ -13,6 +15,16 @@ public class WiiRemoteEventHandler extends WiiRemoteAdapter {
 	
 	@Override
 	public void IRInputReceived(WRIREvent evt) {
-		
+		System.out.println("IRInputReceived");
+	}
+	
+	@Override
+	public void statusReported(WRStatusEvent e) {
+		System.out.println(e.toString());
+	}
+	
+	@Override
+	public void buttonInputReceived(WRButtonEvent e) {
+		System.out.println(e.toString());
 	}
 }
