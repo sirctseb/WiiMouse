0fb6a0b5d6acdc20c4ea8424b2352b21e97376dc
Christopher Best <bestchris@gmail.com>
2012-10-26 14:45:57 -0400

    store delegate; override ir input event

--- src/edu/umich/cjbest/wiimouse/WiiRemoteEventHandler.java
+++ src/edu/umich/cjbest/wiimouse/WiiRemoteEventHandler.java
@@ -1,7 +1,18 @@
 package edu.umich.cjbest.wiimouse;
 
+import wiiremotej.event.WRIREvent;
 import wiiremotej.event.WiiRemoteAdapter;
 
 public class WiiRemoteEventHandler extends WiiRemoteAdapter {
-
+	
+	WiiMouse delegate;
+	
+	WiiRemoteEventHandler(WiiMouse delegate) {
+		this.delegate = delegate;
+	}
+	
+	@Override
+	public void IRInputReceived(WRIREvent evt) {
+		
+	}
 }
