4467f6f2d0ea475d7507d56ec5abeebb2c85eb38
Christopher Best <bestchris@gmail.com>
2012-10-26 10:54:24 -0400

    added src folder with main app file

---
+++ src/edu/umich/cjbest/wiimouse/WiiMouse.java
@@ -0,0 +1,21 @@
+package edu.umich.cjbest.wiimouse;
+
+import org.jdesktop.application.SingleFrameApplication;
+
+public class WiiMouse extends SingleFrameApplication {
+
+	@Override
+	protected void startup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
